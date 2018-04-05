dfb7c90903b639b64895d21503153cffddef1219
Kay Wolter <kay.wolter@funktionswerk.de>
2018-04-05 13:27:11 +0200

    LOG-36 Added test case for log event

---
+++ test/LHLogEventTest.php
@@ -0,0 +1,40 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../src/LogHero.php';
+
+use PHPUnit\Framework\TestCase;
+
+class LHLogEventTest extends TestCase
+{
+    private $logEvent;
+
+    public function setUp() {
+        $this->logEvent = new LHLogEvent();
+    }
+
+    public function testCreateColumns() {
+        $this->assertEquals(count($this->logEvent->columns()), 8);
+    }
+
+    public function testCreateRowFromLogEventData()
+    {
+        $this->logEvent->setIpAddress('123.456.78.9');
+        $this->logEvent->setHostName('www.example.com');
+        $this->logEvent->setLandingPagePath('/home');
+        $this->logEvent->setMethod('GET');
+        $this->logEvent->setStatusCode('200');
+        $this->logEvent->setUserAgent('Firefox');
+        $this->logEvent->setTimestamp(new DateTime('2018-03-31T15:03:01Z'));
+        $this->assertEquals($this->logEvent->row(), [
+            '4355d3ffc1fd8aa45fc712ed92e23081',
+            'www.example.com',
+            '/home',
+            'GET',
+            '200',
+            '2018-03-31T15:03:01+00:00',
+            '3ee9e546c0a3811697e424f94ee70bc1',
+            'Firefox'
+        ]);
+    }
+}
+?>
