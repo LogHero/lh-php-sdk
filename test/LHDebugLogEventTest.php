2747fe76e0282f3c193a9b769ac4c2cddf9a8e99
Kay Wolter <kay.wolter@funktionswerk.de>
2018-04-06 11:52:22 +0200

    LOG-36 Added test for debug log event

---
+++ test/LHDebugLogEventTest.php
@@ -0,0 +1,22 @@
+<?php
+require_once __DIR__ . '/../src/LogHeroDebug.php';
+
+use PHPUnit\Framework\TestCase;
+
+class LHDebugLogEventTest extends TestCase {
+
+    public function testSendsRawIpAddress() {
+        $logEvent = new LHDebugLogEvent();
+        $logEvent
+            ->setIpAddress('123.456.78.9')
+            ->setHostName('www.example.com')
+            ->setLandingPagePath('/home')
+            ->setMethod('GET')
+            ->setStatusCode('200')
+            ->setUserAgent('Firefox')
+            ->setTimestamp(new DateTime('2018-03-31T15:03:01Z'));
+        $this->assertEquals($logEvent->columns()[8], 'rawIp');
+        $this->assertEquals($logEvent->row()[8], '123.456.78.9');
+    }
+
+}
