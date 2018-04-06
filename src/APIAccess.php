dee8008f3d3ad9d15a8304d8fb4d847cfb07ebe3
Kay Wolter <kay.wolter@funktionswerk.de>
2018-04-06 10:40:11 +0200

    LOG-36 Added tests for API access with curl

--- src/APIAccess.php
+++ src/APIAccess.php
@@ -4,6 +4,10 @@ interface APIAccess {
     public function submitLogPackage($payloadAsJson);
 }
 
+class APIAccessException extends Exception {
+    
+}
+
 abstract class APIAccessBase implements APIAccess {
     protected $apiKey;
     protected $apiLogPackageEndpoint;
@@ -15,25 +19,60 @@ abstract class APIAccessBase implements APIAccess {
 
 }
 
-# TODO Class is not tested
-class APIAccessCurl extends APIAccessBase{
+
+class CurlClient {
+    private $curl;
+
+    public function __construct($url) {
+        $this->curl = curl_init($url);
+    }
+
+    public function setOpt($name, $value) {
+        curl_setopt($this->curl, $name, $value);
+    }
+
+    public function exec() {
+        return curl_exec($this->curl);
+    }
+
+    public function getInfo($infoType) {
+        return curl_getinfo($this->curl, $infoType);
+    }
+
+    public function close() {
+        curl_close($this->curl);
+    }
+    
+    public function error() {
+        return curl_error($this->curl);
+    }
+}
+
+
+class APIAccessCurl extends APIAccessBase {
 
     public function submitLogPackage($payloadAsJson) {
-        $curl = curl_init($this->apiLogPackageEndpoint);
-        curl_setopt($curl, CURLOPT_HTTPHEADER,
-            array(
-                'Content-type: application/json',
-                'Authorization: '.$this->apiKey
-            )
-        );
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PUT');
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $payloadAsJson);
-        $response = curl_exec($curl);
-        $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        $curlClient = $this->createCurlClient($this->apiLogPackageEndpoint);
+        $curlClient->setOpt(CURLOPT_HTTPHEADER, array(
+            'Content-type: application/json',
+            'Authorization: '.$this->apiKey
+        ));
+        $curlClient->setOpt(CURLOPT_CUSTOMREQUEST, 'PUT');
+        $curlClient->setOpt(CURLOPT_POSTFIELDS, $payloadAsJson);
+        $curlClient->exec();
+        $status = $curlClient->getInfo(CURLINFO_HTTP_CODE);
         if ( $status >= 300 ) {
-            die('Error: call to URL $url failed with status $status, response $response, curl_error ' . curl_error($curl) . ', curl_errno ' . curl_errno($curl));
+            $errorMessage = $curlClient->error();
+            $curlClient->close();
+            throw new APIAccessException(
+                'Call to URL '.$this->apiLogPackageEndpoint.' failed with status '.$status.'; Message: '.$errorMessage
+            );
         }
-        curl_close($curl);
+        $curlClient->close();
+    }
+
+    protected function createCurlClient($url) {
+        return new CurlClient($url);
     }
 
 }
