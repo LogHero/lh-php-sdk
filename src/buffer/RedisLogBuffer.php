95fc93456901d8c1655e2771b3a1b2f490e64c0d
Kay Wolter <kay.wolter@funktionswerk.de>
2018-08-28 15:25:42 +0200

    LOG-174 Dump if max time interval is reached

--- src/buffer/RedisLogBuffer.php
+++ src/buffer/RedisLogBuffer.php
@@ -1,35 +1,61 @@
 <?php
 namespace LogHero\Client;
 
-use LogHero\Client\RedisOptions;
-
 
 class RedisLogBuffer implements LogBufferInterface {
     private $redisClient;
     private $redisLogBufferKey;
+    private $redisLastDumpBufferKey;
     private $numberOfEventsInBuffer;
+    private $lastDumpTimestamp;
     private $maxNumberOfEventsInBuffer;
+    private $maxDumpTimeIntervalSeconds;
 
-    public function __construct($redisClient, $redisOptions, $maxNumberOfEventsInBuffer=500) {
+    public function __construct(
+        $redisClient,
+        $redisOptions,
+        $maxNumberOfEventsInBuffer=1000,
+        $maxDumpTimeIntervalSeconds=300
+    ) {
         $this->redisClient = $redisClient;
-        $this->redisLogBufferKey = $redisOptions->getRedisKeyPrefix() .':logs';
+        $this->redisLogBufferKey = $redisOptions->getRedisKeyPrefix() . ':logs';
+        $this->redisLastDumpBufferKey = $this->redisLogBufferKey . ':last-dump';
         $this->numberOfEventsInBuffer = 0;
         $this->maxNumberOfEventsInBuffer = $maxNumberOfEventsInBuffer;
+        $this->maxDumpTimeIntervalSeconds = $maxDumpTimeIntervalSeconds;
     }
 
     public function push($logEvent) {
-        $this->numberOfEventsInBuffer = $this->redisClient->lpush($this->redisLogBufferKey, serialize($logEvent));
+        $responses = $this->redisClient
+            ->transaction()
+            ->lpush($this->redisLogBufferKey, serialize($logEvent))
+            ->get($this->redisLastDumpBufferKey)
+            ->execute();
+        $this->numberOfEventsInBuffer = $responses[0];
+        $this->lastDumpTimestamp = (float) $responses[1];
     }
 
     public function needsDumping() {
-        return $this->numberOfEventsInBuffer >= $this->maxNumberOfEventsInBuffer;
+        if($this->numberOfEventsInBuffer >= $this->maxNumberOfEventsInBuffer) {
+            return true;
+        }
+        if ($this->lastDumpTimestamp === null) {
+            return true;
+        }
+        $currentTimestamp = microtime(true);
+        $nextDumpTimestamp = $this->lastDumpTimestamp + $this->maxDumpTimeIntervalSeconds;
+        return $currentTimestamp >= $nextDumpTimestamp;
     }
 
     public function dump() {
-        $responses = $this->redisClient->transaction()
+        $unixTimestamp = microtime(true);
+        $responses = $this->redisClient
+            ->transaction()
+            ->set($this->redisLastDumpBufferKey, $unixTimestamp)
             ->lrange($this->redisLogBufferKey, 0, -1)
-            ->del($this->redisLogBufferKey)->execute();
-        $logEventsSerialized = $responses[0];
+            ->del($this->redisLogBufferKey)
+            ->execute();
+        $logEventsSerialized = $responses[1];
         $logEventsUnserialized = array();
         foreach($logEventsSerialized as $logEvent) {
             $logEventsUnserialized[] = unserialize($logEvent);
--- test/RedisLogBufferTest.php
+++ test/RedisLogBufferTest.php
@@ -4,14 +4,15 @@ namespace LogHero\Client\Test;
 use LogHero\Client\RedisLogBuffer;
 use LogHero\Client\RedisOptions;
 use PHPUnit\Framework\TestCase;
-use Predis\Client;
 
 
 interface RedisClientMockInterface {
     public function lpush($key, $data);
+    public function set($key, $data);
     public function transaction();
     public function lrange($key, $start, $stop);
     public function del($key);
+    public function get($key);
     public function execute();
 }
 
@@ -21,39 +22,62 @@ class RedisLogBufferTest extends TestCase {
     private $logBuffer;
     private $redisClientMock;
     private $maxNumberOfEventsInBuffer = 3;
+    private $maxDumpTimeIntervalSeconds = 4;
+    private $microtimeMock;
+    private static $currentTime = 1523429300.8000;
 
     public function setUp() {
         parent::setUp();
+        $GLOBALS['currentTime'] = static::$currentTime;
+        $this->microtimeMock = createMicrotimeMock();
+        $this->microtimeMock->enable();
         $redisOptions = new RedisOptions('REDIS_URL', 'key-prefix');
         $this->redisClientMock = $this->createMock(RedisClientMockInterface::class);
-        $this->logBuffer = new RedisLogBuffer($this->redisClientMock, $redisOptions, $this->maxNumberOfEventsInBuffer);
+        $this->logBuffer = new RedisLogBuffer(
+            $this->redisClientMock,
+            $redisOptions,
+            $this->maxNumberOfEventsInBuffer,
+            $this->maxDumpTimeIntervalSeconds
+        );
+    }
+
+    public function tearDown() {
+        parent::tearDown();
+        $this->microtimeMock->disable();
     }
 
     public function testPushLogEvent() {
         $logEvent = createLogEvent('/page');
-        $this->redisClientMock
-            ->expects($this->once())
-            ->method('lpush')
-            ->with($this->equalTo('key-prefix:logs'), $this->equalTo(serialize($logEvent)))
-            ->willReturn(1);
+        $this->expectPush($logEvent, 1, static::$currentTime);
         $this->logBuffer->push($logEvent);
     }
 
     public function testNeedsNoDumpingIfWithinMaxBufferSize(){
-        $this->redisClientMock
-            ->expects($this->once())
-            ->method('lpush')
-            ->willReturn(1);
-        $this->logBuffer->push(createLogEvent('/page'));
+        $logEvent = createLogEvent('/page');
+        $this->expectPush($logEvent, 1, static::$currentTime);
+        $this->logBuffer->push($logEvent);
         static::assertFalse($this->logBuffer->needsDumping());
     }
 
     public function testNeedsDumpingIfExceedsMaxBufferSize() {
-        $this->redisClientMock
-            ->expects($this->once())
-            ->method('lpush')
-            ->willReturn(3);
-        $this->logBuffer->push(createLogEvent('/page'));
+        $logEvent = createLogEvent('/page');
+        $this->expectPush($logEvent, 3, static::$currentTime);
+        $this->logBuffer->push($logEvent);
+        static::assertTrue($this->logBuffer->needsDumping());
+    }
+
+    public function testNeedsDumpingIfNoDumpTimestampAvailable() {
+        $logEvent = createLogEvent('/page');
+        $this->expectPush($logEvent, 1, null);
+        $this->logBuffer->push($logEvent);
+        static::assertTrue($this->logBuffer->needsDumping());
+    }
+
+    public function testNeedsDumpingIfMaxDumpTimeIntervalReached() {
+        $logEvent = createLogEvent('/page');
+        $nowMinus5Seconds = static::$currentTime - 5;
+        $this->expectPush($logEvent, 1, $nowMinus5Seconds);
+        $this->logBuffer->push($logEvent);
         static::assertTrue($this->logBuffer->needsDumping());
     }
 
@@ -72,6 +96,7 @@ class RedisLogBufferTest extends TestCase {
             serialize(createLogEvent('/page-2'))
         );
         $responses = array(
+            1,
             $logEventsSerialized
         );
         $this->redisClientMock
@@ -81,11 +106,44 @@ class RedisLogBufferTest extends TestCase {
             ->willReturn($this->redisClientMock);
         $this->redisClientMock
             ->expects($this->once())
+            ->method('set')
+            ->with($this->equalTo('key-prefix:logs:last-dump'), $this->equalTo(1523429300.8000))
+            ->willReturn($this->redisClientMock);
+        $this->redisClientMock
+            ->expects($this->once())
             ->method('execute')
             ->willReturn($responses);
         $logEvents = $this->logBuffer->dump();
         static::assertEquals('/page-1', $logEvents[0]->row()[3]);
         static::assertEquals('/page-2', $logEvents[1]->row()[3]);
+    }
+
+    private function expectPush($logEvent, $lpushReturnValue, $lastDumpTimestamp) {
+        if ($lastDumpTimestamp) {
+            $lastDumpTimestamp = (string) $lastDumpTimestamp;
+        }
+        $this->redisClientMock
+            ->expects($this->once())
+            ->method('transaction')
+            ->willReturn($this->redisClientMock);
+        $this->redisClientMock
+            ->expects($this->once())
+            ->method('lpush')
+            ->with($this->equalTo('key-prefix:logs'), $this->equalTo(serialize($logEvent)))
+            ->willReturn($this->redisClientMock);
+        $this->redisClientMock
+            ->expects($this->once())
+            ->method('get')
+            ->with($this->equalTo('key-prefix:logs:last-dump'))
+            ->willReturn($this->redisClientMock);
+        $responses = array(
+            $lpushReturnValue,
+            $lastDumpTimestamp
+        );
+        $this->redisClientMock
+            ->expects($this->once())
+            ->method('execute')
+            ->willReturn($responses);
     }
 
 }
