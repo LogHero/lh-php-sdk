fc9b6e923bac70c53c2982ea758d7369adc67a3e
Kay Wolter <kay.wolter@funktionswerk.de>
2019-02-26 17:43:08 +1300

    LOG-194 Added async log transport sample with flush

---
+++ sample/hello-world/flush.php
@@ -0,0 +1,48 @@
+<?php
+
+
+// Make sure that we quickly respond to client and close the session before the actual flushing is started
+ignore_user_abort( true );
+set_time_limit(0);
+
+ob_start();
+header('Connection: close');
+header('Content-Length: '.ob_get_length());
+header('Content-Encoding: none');
+ob_end_flush();
+ob_flush();
+flush();
+
+if(session_id()) {
+    session_write_close();
+}
+
+
+// Start flushing the buffered log events
+require_once __DIR__ . '/../../autoload.php';
+
+use LogHero\Client\APISettings;
+use LogHero\Client\APIAccess;
+use LogHero\Client\FileLogBuffer;
+use LogHero\Client\AsyncLogTransport;
+
+
+$apiKey = 'YOUR_API_KEY';
+$clientId = 'Hello-World-Sample';
+$apiSettings = new APISettings($apiKey);
+
+// Flush token is used to ensure that this flush action is authorized
+$flushToken = $_SERVER['HTTP_TOKEN'];
+if ($flushToken !== $apiSettings->getKey()) {
+    throw new Exception('Token is invalid');
+}
+
+// Initialize log buffer to read buffered log events
+$logBuffer = new FileLogBuffer(__DIR__ . '/buffer.loghero.io');
+
+// Initialize API client to transform, compress and submit log events to LogHero API;
+$apiAccess = new APIAccess($clientId, $apiSettings);
+
+// Initialize log transport and dump buffered log events to the LogHero API
+$logTransport = new AsyncLogTransport($logBuffer, $apiAccess, $clientId, $apiKey, null);
+$logTransport->dumpLogEvents();
--- sample/hello-world/index.php
+++ sample/hello-world/index.php
@@ -1,6 +1,6 @@
 <html>
   <head>
-    <title>LogHero Hello World</title>
+    <title>LogHero Example Page</title>
   </head>
 
   <body>
@@ -16,31 +16,39 @@
   use LogHero\Client\APIAccess;
   use LogHero\Client\FileLogBuffer;
   use LogHero\Client\LogTransport;
+  use LogHero\Client\AsyncLogTransport;
   use LogHero\Client\LogEventFactory;
 
   $apiKey = 'YOUR_API_KEY';
   $clientId = 'Hello-World-Sample';
+  $flushTriggerEndpoint = 'http://localhost/sdk/sample/hello-world/flush.php';
   $apiSettings = new APISettings($apiKey);
 
+  // To avoid heavy load on the LogHero API and your server, log events are supposed to be sent in batches.
+  // The simplest way of buffering is creating a text file with the serialized log events.
+  // If you server load does not allow the usage of a buffer file you can switch to using the RedisLogBuffer instead.
   echo '<p>Initialize log buffer to collect hits for batch processing</p>';
   $logBuffer = new FileLogBuffer(__DIR__ . '/buffer.loghero.io');
 
-  echo '<p>Initialize API client to submit log events to LogHero API</p>';
+  echo '<p>Initialize API client to transform, compress and submit log events to LogHero API</p>';
   $apiAccess = new APIAccess($clientId, $apiSettings);
 
-  // TODO Provide sync and async sample
+  // The SDK provides two log transports:
+  // - LogTransport: The simplest log transport that sends the log events immediately whenever the log buffer needs dumping.
+  // - AsyncLogTransport: This log transport dumps the log events asynchronically.
+  //                      You need to provide a flush endpoint that does the actual flushing (see './flush.php').
+  //                      Whenever the buffer needs dumping, the log transport hits this endpoint to trigger the flush.
   echo '<p>Initialize log transport to define when log events are sent to the LogHero API</p>';
-  $logTransport = new LogTransport($logBuffer, $apiAccess);
+  #$logTransport = new LogTransport($logBuffer, $apiAccess);
+  $logTransport = new AsyncLogTransport($logBuffer, $apiAccess, $clientId, $apiKey, $flushTriggerEndpoint);
 
   echo '<p>Create log event from the request and server information available</p>';
   $logEventFactory = new LogEventFactory();
   $logEvent = $logEventFactory->create();
 
-  echo '<p>
-     Submit log event to buffer.
-     When the maximum buffer size is reached or the age of the oldest log event in the buffer exceeds a time limit,
-     all log events from the buffer are sent as one batch to the LogHero API.
-  </p>';
+  // When the maximum buffer size is reached or the age of the oldest log event in the buffer exceeds a time limit,
+  // all log events from the buffer are sent automatically as one batch to the LogHero API.
+  echo '<p>Submit log event to buffer.</p>';
   $logTransport->submit($logEvent);
 
   echo '<p>Page loaded successfully</p>';
